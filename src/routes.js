7653ca34888b8e363a92349674a76e70ef2e2747
hazel260802 <lehuyentrang260802@example.com>
2023-06-13 10:58:45 +0700

    refactoring

--- src/routes.js
+++ src/routes.js
@@ -49,25 +49,25 @@ const router = createBrowserRouter([
               { path: 'settings', element: <SettingPage /> },
             ],
           },
+          {
+            path: 'login',
+            element: <LoginPage />,
+          },
+          {
+            element: <SimpleLayout />,
+            children: [
+              { element: <Navigate to="/dashboard/app" />, index: true },
+              { path: '404', element: <Page404 /> },
+              { path: '*', element: <Navigate to="/404" /> },
+            ],
+          },
+          {
+            path: '*',
+            element: <Navigate to="/404" replace />,
+          },
         ],
       },
     ],
-  },
-  {
-    path: 'login',
-    element: <LoginPage />,
-  },
-  {
-    element: <SimpleLayout />,
-    children: [
-      { element: <Navigate to="/dashboard/app" />, index: true },
-      { path: '404', element: <Page404 /> },
-      { path: '*', element: <Navigate to="/404" /> },
-    ],
-  },
-  {
-    path: '*',
-    element: <Navigate to="/404" replace />,
   },
 ]);
 export default router;
